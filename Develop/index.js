8c7a60cae93a8f3cde0419bbd4c9d0232b105d67
jbeedle19 <josh.beedle@gmail.com>
2020-12-30 10:55:17 -0500

    started inquirer prompt

--- Develop/index.js
+++ Develop/index.js
@@ -8,16 +8,53 @@ const questions = [];
 const promptUser = () => {
     return inquirer.prompt([
         {
-            type: 
-        }
-    ])
-}
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a title for your project!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project.',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please provide a description for your project!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Provide installation instructions for your project',
+            validate: installationInput => {
+                if (installationInput) {
+                    return true;
+                } else {
+                    console.log('Please provide installation instructions for your project!');
+                }
+            }
+        },
+    ]);
+};
 
+promptUser()
+    .then(data => {
+        console.log(data);
+    });
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() {}
 
 // Function call to initialize app
-init();
+//init();
