6823c51c4258ac878f1715c3d21345e8b3f5ca3e
jbeedle19 <josh.beedle@gmail.com>
2020-12-30 11:32:53 -0500

    finished the questions prompt with inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -34,7 +34,7 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'installation',
-            message: 'Provide installation instructions for your project',
+            message: 'Provide installation instructions for your project.',
             validate: installationInput => {
                 if (installationInput) {
                     return true;
@@ -43,6 +43,72 @@ const promptUser = () => {
                 }
             }
         },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide usage instructions for your project.',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log('Please provide usage instructions for your project!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Provide contribution guidelines for your project.',
+            validate: contributingInput => {
+                if (contributingInput) {
+                    return true;
+                } else {
+                    console.log('Please provide contribution guidelines for your project!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Provide testing instructions for your project.',
+            validate: testInput => {
+                if (testInput) {
+                    return true;
+                } else {
+                    console.log('Please provide testing instructions for your project!');
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Which license does your project use?',
+            choices: ['MIT', 'Apache', 'GPLv2', 'BSD 3-clause', 'GPLv3', 'No License']
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'What is your GitHub username?',
+            validate: usernameInput => {
+                if (usernameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your username!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your email address!');
+                }
+            }
+        }
     ]);
 };
 
