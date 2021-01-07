c53ffb9b8647cf723fc41c9fd7efe0c7e23a539c
jbeedle19 <josh.beedle@gmail.com>
2021-01-07 11:18:02 -0500

    removed defaults from prompt

--- Develop/index.js
+++ Develop/index.js
@@ -10,7 +10,6 @@ const promptUser = () => {
             type: 'input',
             name: 'title',
             message: 'What is the title of your project?',
-            default: 'README Generator',
             validate: titleInput => {
                 if (titleInput) {
                     return true;
@@ -23,7 +22,6 @@ const promptUser = () => {
             type: 'input',
             name: 'description',
             message: 'Provide a description of the project.',
-            default: 'Generates a README file based off the answers that the user provides.',
             validate: descriptionInput => {
                 if (descriptionInput) {
                     return true;
@@ -36,7 +34,6 @@ const promptUser = () => {
             type: 'input',
             name: 'installation',
             message: 'Provide installation instructions for your project.',
-            default: 'Download the zip file or copy the repo to your computer.',
             validate: installationInput => {
                 if (installationInput) {
                     return true;
@@ -49,7 +46,6 @@ const promptUser = () => {
             type: 'input',
             name: 'usage',
             message: 'Provide usage instructions for your project.',
-            default: 'Download the file and navigate into the folder through the command line. Type "npm start" in order to run the application. Answer each question and upon completion open the README file created in the dist folder. Watch the following video for further explanation (video link).',
             validate: usageInput => {
                 if (usageInput) {
                     return true;
@@ -62,7 +58,6 @@ const promptUser = () => {
             type: 'input',
             name: 'contributing',
             message: 'Provide contribution guidelines for your project.',
-            default: 'In order to contribute you can do X, Y, and Z.',
             validate: contributingInput => {
                 if (contributingInput) {
                     return true;
@@ -75,7 +70,6 @@ const promptUser = () => {
             type: 'input',
             name: 'tests',
             message: 'Provide testing instructions for your project.',
-            default: 'There is a test that tests X and you can type Y to run it.',
             validate: testInput => {
                 if (testInput) {
                     return true;
@@ -94,7 +88,6 @@ const promptUser = () => {
             type: 'input',
             name: 'username',
             message: 'What is your GitHub username?',
-            default: 'jbeedle19',
             validate: usernameInput => {
                 if (usernameInput) {
                     return true;
@@ -107,7 +100,6 @@ const promptUser = () => {
             type: 'input',
             name: 'email',
             message: 'What is your email address?',
-            default: 'josh.beedle@gmail.com',
             validate: emailInput => {
                 if (emailInput) {
                     return true;
